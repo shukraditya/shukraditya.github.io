b26e976c8d539a54712de517a1eccc8df38bab54
Shukraditya <shukra12bose@gmail.com>
2026-04-28 05:20:13 +0530

    few changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwind from '@astrojs/tailwind';
 import { remarkWikiLinks } from './src/plugins/remarkWikiLinks.ts';
 import { remarkObsidianLists } from './src/plugins/remarkObsidianLists.ts';
 import { remarkCallouts } from './src/plugins/remarkCallouts.ts';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shukraditya.in',
   output: 'static',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   markdown: {
     remarkPlugins: [remarkCallouts, remarkObsidianLists, remarkWikiLinks],
   },
